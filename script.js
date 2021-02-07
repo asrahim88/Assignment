7fc024c715cb9fa82cd002dff3db85febf83046c
asrahim88 <tasmimhasan1997@gmail.com>
2021-02-07 21:51:29 +0600

    total update without dynamic list

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
 
 document.getElementById('searchBtn').addEventListener('click', () => {
     const searchName = document.getElementById('inputBtn').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`)
+    if (searchName !== '') {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`)
+        .then(response => response.json())
+        .then(data => displayMeal(data.meals));
+    } else {
+        alert("please insert your favorite meal in this search box");
+    }
+})
 
-    .then(response => response.json())
-    .then(data => displayMeal(data.meals));
-
-    
-    const displayMeal = food => {
-        // console.log(food);
+const displayMeal = food => {
+    if (food !== null) {
         food.forEach(meal => {
             const mealArea = document.createElement('div');
             mealArea.className = 'mealArea';
             const mealInfo = `
             <div onClick = 'displayMealDetails("${meal.strMeal}") '>
-            <img src="${meal.strMealThumb}">
-            <p>'${meal.strMeal}'</p>
+            <img src=${meal.strMealThumb}>
+            <p>${meal.strMeal}</p>
             </div>
             `
             mealArea.innerHTML = mealInfo;
             
-
             const mainContainer = document.getElementById('mainContainer');
             mainContainer.appendChild(mealArea);
 
+            const notFound = document.getElementById('notFoundContainer');
+            notFound.style.display = 'none';
         });
+    } else {
+        const notFound = document.getElementById('notFoundContainer');
+        notFound.style.display = 'block';
     }
+}
 
-})
 
 const displayMealDetails = details => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${details}`
-    console.log("second url", url)
     fetch(url)
     .then(response => response.json())
+    // .then(data => console.log(data))
     .then(data => renderMealInformation(data.meals[0]))
+    
 }
 
 const renderMealInformation = (meals) => {
-    console.log('rander information',meals)
+    console.log('meals object',meals);
+    const popUpMeal = document.getElementById('mealDetails');
+    popUpMeal.innerHTML = `
+    <div class = "mealDetailsBox">
+    <img  src=${meals.strMealThumb} alt="">
+    <h1>${meals.strMeal}</h1>
+    <h4>Ingredients </h4>
+    <li>${meals.strIngredient1}:  <span> ${meals.strMeasure1} </span></li>
+    <li>${meals.strIngredient2}: <span> ${meals.strMeasure2} </span></li>
+    <li>${meals.strIngredient3}: <span> ${meals.strMeasure3} </span></li>
+    <li>${meals.strIngredient4}: <span> ${meals.strMeasure4} </span></li>
+    <li>${meals.strIngredient5}: <span> ${meals.strMeasure5} </span></li>
+    <li>${meals.strIngredient6}: <span> ${meals.strMeasure6} </span></li>
+    <li>${meals.strIngredient7}: <span> ${meals.strMeasure7} </span></li>
+    <li>${meals.strIngredient8}: <span> ${meals.strMeasure8} </span></li>
+    <li>${meals.strIngredient9}: <span> ${meals.strMeasure9} </span></li>
+    </div>
+   
+    ` 
 }
 
 
@@ -56,28 +82,5 @@ const renderMealInformation = (meals) => {
 
 
 
-// // fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=a')
-// fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
-// .then(response => response.json())
-// .then(data => displayMeal(data))
-// // .then(data => displayMeal(data.meals))
 
-
-// function displayMeal(food) {
-//     console.log(food);
-//     // food.forEach(meal => {
-//     //     const mealArea = document.createElement('div');
-//     //     mealArea.className = 'mealArea';
-//     //     const mealInfo = `
-//     //     <img src="${meal.strMealThumb}">
-//     //    <p>'${meal.strMeal}'</p>
-//     //     `
-//     //     mealArea.innerHTML = mealInfo;
-        
-
-//     //     const mainContainer = document.getElementById('mainContainer');
-//     //     mainContainer.appendChild(mealArea);
-
-//     // });
-// }
 
