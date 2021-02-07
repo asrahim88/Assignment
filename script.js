88c79f5445f81716906682f75757ce278293e3db
asrahim88 <tasmimhasan1997@gmail.com>
2021-02-07 14:39:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    const searchName = document.getElementById('inputBtn').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`)
+
+    .then(response => response.json())
+    .then(data => displayMeal(data.meals));
+
+    
+    const displayMeal = food => {
+        // console.log(food);
+        food.forEach(meal => {
+            const mealArea = document.createElement('div');
+            mealArea.className = 'mealArea';
+            const mealInfo = `
+            <div onClick = 'mealDetails("${meal.strMeal}") '>
+            <img src="${meal.strMealThumb}">
+            <p>'${meal.strMeal}'</p>
+            </div>
+            `
+            mealArea.innerHTML = mealInfo;
+            
+
+            const mainContainer = document.getElementById('mainContainer');
+            mainContainer.appendChild(mealArea);
+
+        });
+    }
+
+})
+
+const mealDetails = details => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${details}`
+    console.log("second url", url)
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data.meals))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=a')
+// fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
+// .then(response => response.json())
+// .then(data => displayMeal(data))
+// // .then(data => displayMeal(data.meals))
+
+
+// function displayMeal(food) {
+//     console.log(food);
+//     // food.forEach(meal => {
+//     //     const mealArea = document.createElement('div');
+//     //     mealArea.className = 'mealArea';
+//     //     const mealInfo = `
+//     //     <img src="${meal.strMealThumb}">
+//     //    <p>'${meal.strMeal}'</p>
+//     //     `
+//     //     mealArea.innerHTML = mealInfo;
+        
+
+//     //     const mainContainer = document.getElementById('mainContainer');
+//     //     mainContainer.appendChild(mealArea);
+
+//     // });
+// }
+
